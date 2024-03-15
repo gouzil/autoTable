2088551ba0bdaf472b133e96651f4e6b5770cfa4
gouzi <530971494@qq.com>
2024-03-15 23:39:27 +0800

    fix utf8

--- autotable/constant.py
+++ autotable/constant.py
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-import platform
-
 APPNAME = "autotable"
 APPAUTHOR = "gouzil"
-if platform.system() == "Windows":
-    CONSOLE_SUCCESSFUL = "v"
-    CONSOLE_ERROR = "X"
-else:
-    CONSOLE_SUCCESSFUL = "✓"
-    CONSOLE_ERROR = "✗"
+CONSOLE_SUCCESSFUL = "✓"
+CONSOLE_ERROR = "✗"
