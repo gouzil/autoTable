9e5362cf352a8ac8641d51734e6cb7630ff9a1ca
gouzil <66515297+gouzil@users.noreply.github.com>
2024-03-16 00:08:15 +0800

    add doctor (#17)

--- autotable/__main__.py
+++ autotable/__main__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import shutil
 from pathlib import Path
 
 import typer
@@ -11,6 +12,7 @@ from rich.style import Style
 
 from autotable.api.issues import get_issues
 from autotable.command import backup, update_content, update_stats
+from autotable.constant import CONSOLE_ERROR, CONSOLE_SUCCESSFUL
 from autotable.utils.appdirs import data_dir, log_dir
 from autotable.utils.fetcher import Fetcher
 
@@ -107,8 +109,56 @@ def clean():
     """
     清理备份文件
     """
-    Path(data_dir()).rmdir()
-    Path(log_dir()).rmdir()
+    if Path(data_dir()).exists():
+        shutil.rmtree(data_dir())
+    if Path(log_dir()).exists():
+        shutil.rmtree(log_dir())
+    Console().print(rf"[green]\[{CONSOLE_SUCCESSFUL}][/green] Cleanup successful")
+
+
+@app.command()
+def doctor():
+    """
+    自检查
+    """
+
+    # 计算应用数据大小
+    try:
+        data_size = 0
+        data_path = Path(data_dir())
+        if not data_path.exists():
+            data_path.mkdir(parents=True, exist_ok=True)
+        for files in data_path.iterdir():
+            if files.is_file():
+                data_size += files.stat().st_size
+        Console().print(
+            rf"[green]\[{CONSOLE_SUCCESSFUL}][/green] Data dir path: {data_dir()}, size: {data_size/1024/1024:.3f}M"
+        )
+    except Exception:
+        Console().print(rf"[red]\[{CONSOLE_ERROR}][/red] Data dir path: {data_dir()}, fail to write to file")
+
+    try:
+        log_size = 0
+        log_path = Path(log_dir())
+        if not log_path.exists():
+            log_path.mkdir(parents=True, exist_ok=True)
+        for files in log_path.iterdir():
+            if files.is_file():
+                log_size += files.stat().st_size
+        Console().print(
+            rf"[green]\[{CONSOLE_SUCCESSFUL}][/green] Log dir path: {log_dir()}, size: {log_size/1024/1024:.3f}M"
+        )
+    except Exception:
+        Console().print(rf"[red]\[{CONSOLE_ERROR}][/red] Log dir path: {log_dir()}, fail to write to file")
+
+    # check github
+    try:
+        Fetcher.set_github("")
+        Fetcher.set_repo("gouzil/autoTable")
+        Fetcher.get_issue(10).body  # noqa: B018
+        Console().print(rf"[green]\[{CONSOLE_SUCCESSFUL}][/green] Github resources")
+    except Exception:
+        Console().print(rf"[red]\[{CONSOLE_ERROR}][/red] Github resources")
 
 
 if __name__ == "__main__":  # pragma: no cover
--- autotable/constant.py
+++ autotable/constant.py
@@ -2,3 +2,5 @@ from __future__ import annotations
 
 APPNAME = "autotable"
 APPAUTHOR = "gouzil"
+CONSOLE_SUCCESSFUL = "✓"
+CONSOLE_ERROR = "✗"
