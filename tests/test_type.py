39838262391c965a207bba9c33a6963603dd484d
gouzi <530971494@qq.com>
2024-03-27 20:50:22 +0800

    fix 🚧 > 🙋

--- tests/test_type.py
+++ tests/test_type.py
@@ -19,6 +19,8 @@ def test_autotable_type_compare():
     assert not StatusType.NEXT_STAGE > StatusType.COMPLETED
     # 🙋 > 🔵
     assert StatusType.CLAIMED > StatusType.PENDING
+    # 🚧 > 🙋
+    assert StatusType.REPAIRING > StatusType.CLAIMED
 
 
 def test_PrType():
