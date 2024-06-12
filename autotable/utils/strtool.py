62f15c8e8f7f2a487d293bd85650bc56b884ab96
megemini <megemini@outlook.com>
2024-06-12 20:40:02 +0800

    ✨ feat: add clean and format title before process (#28)

--- autotable/utils/strtool.py
+++ autotable/utils/strtool.py
@@ -1,13 +1,46 @@
 from __future__ import annotations
 
+import re
+
 # 定义中文符号
-chinese_character = r"，。！？；：()《》【】“”\‘\’"  # noqa: RUF001
+chinese_character = r"、，。！？；：()《》【】“”\‘\’"  # noqa: RUF001
 # 定义对应的英文符号
-english_character = r',.!?;:()<>[]""\'\''
+english_character = r',,.!?;:()<>[]""\'\''
 
 
 _translate_table = str.maketrans(chinese_character, english_character)
 
 
+pat_title = re.compile(r"(?P<prefix>[a-zA-Z]+)[^\[]*?((?P<no>(\d+))|(\[(?P<no_start>\d+)-(?P<no_end>\d+)\]))")
+
+
 def str_translate(input: str) -> str:
     return input.translate(_translate_table)
+
+
+def clean_title(title: str) -> str:
+    if title is not None and not title.strip():
+        return title
+
+    title = str_translate(title)
+
+    if title.lower().startswith("no."):
+        # just return the title: No.1,3-4
+        return title
+
+    # format title:
+    # A15 -> A-15
+    # A+2,A*4，A - 1、A- 24,A234\a432 -> A-2,A-4,A-1,A-24,A-234,A432 # noqa: RUF003
+    # A-[48-52] -> A-48,A-49,A-50,A-51,A-52
+    _title = []
+    for match in pat_title.finditer(title):
+        if match.group("no"):
+            _title.append(match.group("prefix").upper() + "-" + match.group("no"))
+        elif match.group("no_start"):
+            no_start = int(match.group("no_start"))
+            no_end = int(match.group("no_end"))
+            for index in range(no_start, no_end + 1):
+                _title.append(match.group("prefix").upper() + "-" + str(index))
+        else:
+            pass
+    return ",".join(_title) or title
--- tests/test_github_title.py
+++ tests/test_github_title.py
@@ -35,6 +35,28 @@ def test_multiple_single_title():
 def test_mix_title():
     title1 = "1, 2-4, 6"
     title2 = "1、 2-4、 6"
+    title3 = "A-[48-52]"
 
     assert titleBase(title1).distribution_parser().mate() == ["1", "2", "3", "4", "6"]
     assert titleBase(title2).distribution_parser().mate() == ["1", "2", "3", "4", "6"]
+    assert titleBase(title3).distribution_parser().mate() == ["A-48", "A-49", "A-50", "A-51", "A-52"]
+
+
+# 常见的几种错别字测试
+def test_typo():
+    title1 = "A+2"
+    title2 = "A*4"
+    title3 = "A - 1"
+    title4 = "A- 24"
+    title5 = "A234"
+    title6 = "a432"
+
+    title_all = r"A+2,A*4，A - 1、A- 24,A234\a432"  # noqa: RUF001
+
+    assert titleBase(title1).distribution_parser().mate() == ["A-2"]
+    assert titleBase(title2).distribution_parser().mate() == ["A-4"]
+    assert titleBase(title3).distribution_parser().mate() == ["A-1"]
+    assert titleBase(title4).distribution_parser().mate() == ["A-24"]
+    assert titleBase(title5).distribution_parser().mate() == ["A-234"]
+    assert titleBase(title6).distribution_parser().mate() == ["A-432"]
+    assert titleBase(title_all).distribution_parser().mate() == ["A-2", "A-4", "A-1", "A-24", "A-234", "A-432"]
